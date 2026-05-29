8f4f1de1837c0a5b0fb92315eb5c5c437eaf94f3
User <user@localhost>
2026-05-29 19:33:59 +0200

    Add Tank War mode to the main menu

--- scenes/ui/ModeSelection.gd
+++ scenes/ui/ModeSelection.gd
@@ -2,3 +2,6 @@ extends Control
 
 func _on_pong_pressed():
 	get_tree().change_scene_to_file("res://scenes/pong/PongArena.tscn")
+
+func _on_tank_war_pressed():
+	get_tree().change_scene_to_file("res://scenes/tank_war/TankWar.tscn")
